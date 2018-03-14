be14f6eacf65b4d515c6a369c2a3360778875997
MLameg <MLameg12@gmail.com>
2018-03-14 18:47:57 -0400

    Update: Layout

--- app/src/main/java/com/example/montikarlo/clicker/MainActivity.java
+++ app/src/main/java/com/example/montikarlo/clicker/MainActivity.java
@@ -1,25 +1,18 @@
 package com.example.montikarlo.clicker;
 
-import android.content.DialogInterface;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    int counterInt = 0;
-    EditText timer_ET;
-    int userTime = 0;
-    TextView counterDisplay;
-    TextView timerDisplay;
-    private static final String FORMAT = "%02d:%02d:%02d";
-    private static final String counterFormat ="%03d";
 
 
     @Override
@@ -27,54 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timer_ET = findViewById(R.id.timerET);
-        counterDisplay = findViewById(R.id.counterDisplay);
-        timerDisplay = findViewById(R.id.timerDisplay);
-
-        Button timerBtn = findViewById(R.id.timerBtn);
-        Button counterBtn = findViewById(R.id.counterBtn);
-
-        userTime = Integer.parseInt(timer_ET.getText().toString());
+        final NumberPicker numPickMin = findViewById(R.id.numberPicker_min);
+        NumberPicker numPickSec = findViewById(R.id.numberPicker_sec);
 
 
+        numPickMin.setMinValue(0);
+        numPickMin.setMaxValue(12);
+        numPickMin.setValue(0);
+        numPickMin.setWrapSelectorWheel(false);
 
+        numPickSec.setMinValue(00);
+        numPickSec.setMaxValue(59);
+        numPickSec.setValue(00);
+        numPickSec.setWrapSelectorWheel(false);
 
-        timerBtn.setOnClickListener(new View.OnClickListener() {
+        Button btn = findViewById(R.id.btn);
+        final TextView tv = findViewById(R.id.tv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                new CountDownTimer(userTime, 1000){
-                    public void onTick(long millisUntilFinished) {
-                        timerDisplay.setText(""
-                                +String.format(FORMAT,
-                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
-                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) -
-                                        TimeUnit.HOURS.toMinutes(
-                                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
-                                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
-                                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
-                    }
-
-                    public void onFinish() {
-                        timerDisplay.setText("done!");
-                    }
-
-                }.start();
-
+                tv.setText(String.valueOf(numPickMin.getValue()));
             }
         });
 
 
 
 
-        counterBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counterInt++;
-                counterDisplay.setText(String.valueOf(String.format(counterFormat,counterInt)));
 
-            }
-        });
 
 
     }
---
+++ app/src/main/java/com/example/montikarlo/clicker/TimerActivity.java
@@ -0,0 +1,84 @@
+package com.example.montikarlo.clicker;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by MontiKarlo on 3/10/2018.
+ */
+
+public class TimerActivity extends AppCompatActivity{
+
+    int counterInt = 0;
+    EditText timer_ET;
+    int userTime = 0;
+    TextView counterDisplay;
+    TextView timerDisplay;
+    private static final String FORMAT = "%02d:%02d:%02d";
+    private static final String counterFormat ="%03d";
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        timer_ET = findViewById(R.id.timerET);
+        counterDisplay = findViewById(R.id.counterDisplay);
+        timerDisplay = findViewById(R.id.timerDisplay);
+
+        Button timerBtn = findViewById(R.id.timerBtn);
+        Button counterBtn = findViewById(R.id.counterBtn);
+
+        userTime = Integer.parseInt(timer_ET.getText().toString());
+
+
+
+
+        timerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new CountDownTimer(userTime, 1000){
+                    public void onTick(long millisUntilFinished) {
+                        timerDisplay.setText(""
+                                +String.format(FORMAT,
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) -
+                                        TimeUnit.HOURS.toMinutes(
+                                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+                    }
+
+                    public void onFinish() {
+                        timerDisplay.setText("done!");
+                    }
+
+                }.start();
+
+            }
+        });
+
+
+
+
+        counterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counterInt++;
+                counterDisplay.setText(String.valueOf(String.format(counterFormat,counterInt)));
+
+            }
+        });
+
+
+
+    }
+
+}
