f29cade4eb5d1242a622d6d13eb690a30f08eb2d
MLameg <MLameg12@gmail.com>
2018-03-14 20:56:20 -0400

    Update: ValueListener

--- app/src/main/java/com/example/montikarlo/clicker/MainActivity.java
+++ app/src/main/java/com/example/montikarlo/clicker/MainActivity.java
@@ -11,44 +11,115 @@ import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NumberPicker.OnValueChangeListener {
 
 
+    NumberPicker numRoundMin;
+    NumberPicker numRoundSec;
+    NumberPicker numBreakMin;
+    NumberPicker numBreakSec;
+    NumberPicker roundCount;
+    TextView displayTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final NumberPicker numPickMin = findViewById(R.id.numberPicker_min);
-        NumberPicker numPickSec = findViewById(R.id.numberPicker_sec);
+        // Grab Number Pickers ---------------------------------------------
+        numRoundMin = findViewById(R.id.numberPicker_min);
+        numRoundSec = findViewById(R.id.numberPicker_sec);
+        numBreakMin = findViewById(R.id.breakPicker_min);
+        numBreakSec = findViewById(R.id.breakPicker_sec);
+        roundCount = findViewById(R.id.roundCountPicker);
+
+        // Sets OnValueChangeListener
+        numRoundMin.setOnValueChangedListener(this);
+        numRoundSec.setOnValueChangedListener(this);
+        numBreakMin.setOnValueChangedListener(this);
+        numBreakSec.setOnValueChangedListener(this);
+        roundCount.setOnValueChangedListener(this);
+
+        // Round NumberPicker Values ---------------------------------------
+        numRoundMin.setMinValue(0);
+        numRoundMin.setMaxValue(12);
+        numRoundMin.setValue(0);
+        numRoundMin.setWrapSelectorWheel(true);
+
+        numRoundSec.setMinValue(00);
+        numRoundSec.setMaxValue(59);
+        numRoundSec.setValue(00);
+        numRoundSec.setWrapSelectorWheel(false);
+
+        // Break NumberPicker Values ---------------------------------------
+        numBreakMin.setMinValue(0);
+        numBreakMin.setMaxValue(10);
+        numBreakMin.setValue(0);
+        numBreakMin.setWrapSelectorWheel(false);
+
+        numBreakSec.setMinValue(00);
+        numBreakSec.setMaxValue(59);
+        numBreakSec.setValue(00);
+        numBreakSec.setWrapSelectorWheel(false);
+
+        // Round Counter NumberPicker Values
+        roundCount.setMinValue(0);
+        roundCount.setMaxValue(12);
+        roundCount.setValue(0);
+        roundCount.setWrapSelectorWheel(true);
 
 
-        numPickMin.setMinValue(0);
-        numPickMin.setMaxValue(12);
-        numPickMin.setValue(0);
-        numPickMin.setWrapSelectorWheel(false);
 
-        numPickSec.setMinValue(00);
-        numPickSec.setMaxValue(59);
-        numPickSec.setValue(00);
-        numPickSec.setWrapSelectorWheel(false);
 
-        Button btn = findViewById(R.id.btn);
-        final TextView tv = findViewById(R.id.tv);
+        displayTotal = findViewById(R.id.totalDisplay);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        numRoundSec.setFormatter(new NumberPicker.Formatter() {
             @Override
-            public void onClick(View v) {
-                tv.setText(String.valueOf(numPickMin.getValue()));
+            public String format(int value) {
+                return String.format("%02d" , value);
             }
         });
 
+        /*
+        numRoundMin.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                tv.setText(String.valueOf(numRoundMin.getValue() + numBreakSec.getValue()));
+            }
+        });
+        */
+    }
 
-
-
-
-
-
+    @Override
+    public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+        switch (picker.getId()){
+            case R.id.numberPicker_min:
+                displayTotal.setText(String.valueOf(((numRoundMin.getValue() +
+                        numRoundSec.getValue() + numBreakMin.getValue() +
+                        numBreakSec.getValue())* roundCount.getValue())));
+                break;
+            case R.id.numberPicker_sec:
+                displayTotal.setText(String.valueOf(((numRoundMin.getValue() +
+                        numRoundSec.getValue() + numBreakMin.getValue() +
+                        numBreakSec.getValue())* roundCount.getValue())));
+                break;
+            case R.id.breakPicker_min:
+                displayTotal.setText(String.valueOf(((numRoundMin.getValue() +
+                        numRoundSec.getValue() + numBreakMin.getValue() +
+                        numBreakSec.getValue())* roundCount.getValue())));
+                break;
+            case R.id.breakPicker_sec:
+                displayTotal.setText(String.valueOf(((numRoundMin.getValue() +
+                        numRoundSec.getValue() + numBreakMin.getValue() +
+                        numBreakSec.getValue())* roundCount.getValue())));
+                break;
+            case R.id.roundCountPicker:
+                displayTotal.setText(String.valueOf(((numRoundMin.getValue() +
+                        numRoundSec.getValue() + numBreakMin.getValue() +
+                        numBreakSec.getValue())* roundCount.getValue())));
+                break;
+            default:
+                break;
+        }
     }
 }
